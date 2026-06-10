402a06f3bf13a46d9fbc9cbea5c4dc0ad2b9f4c1
mahendar-07 <mahendar10019@gmail.com>
2026-06-10 19:00:52 +0530

    Move JWT  secret to environment variables

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,23 +4,26 @@ import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { User } from '../users/entities/user.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
-
-    JwtModule.register({
-      secret: 'mysecretkey',
-      signOptions: {
-        expiresIn: '1d',
-      },
-    }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET')!,
+        signOptions: {
+          expiresIn: '1d',
+        },
+      }),
+}),
   ],
-  controllers: [AuthController],
+controllers: [AuthController],
   providers: [AuthService,
     JwtStrategy,
   ],
 })
-export class AuthModule {}
+export class AuthModule { }
--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -6,11 +6,10 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
-      jwtFromRequest:
-        ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
-      secretOrKey: 'mysecretkey',
-    });
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  ignoreExpiration: false,
+  secretOrKey: process.env.JWT_SECRET!,
+});
   }
 
   async validate(payload: any) {
